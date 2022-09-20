e44c58cc977fdd53052f0cb92e9defa256ae629f
Erick Gonzalez <deweyerick@gmail.com>
2022-09-20 12:47:41 -0500

    bootstrap local install, no js file, jQuery, and fontawesome through link

--- app.py
+++ app.py
@@ -5,31 +5,6 @@ import os
 
 app = Flask(__name__)
 
-#   NAV MODULE
-#   Install pip3 install Flask-Navigation
-from flask_navigation import Navigation
-nav = Navigation(app)
-
-"""#
-# @nav.Bar 
-#   arg1: string name
-#   arg2: array items
-#   info: Initializing name of navigation 
-#                class with items definition.
-# @nav.Item
-#   arg1: string label
-#   arg2: string url
-#   arg3: dicctionary arguments
-#   info: Assigns items or lists for each bar. 
-#         Label name, its linked page URL and 
-#         extra args like parameter and value 
-#         of dictionary.
- """
-nav.Bar('top', [
-    nav.Item('Home', 'home'),
-    nav.Item('Proyectos', 'projects')
-])
-
 # GET website DATA with JSON
 import json
 f = open('static/data.json')
@@ -66,14 +41,6 @@ def home():
             'home.html',
             data = data,
             images = images)
-
-@app.route('/projects')
-def projects():
-    return render_template(
-        'projects.html',
-        data = data,
-        images = images
-        )
 
 #   Run 
 if __name__ =='__main__':

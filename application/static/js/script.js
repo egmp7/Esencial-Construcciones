dca9f81c1986cb6364185e39e58ed0ee30aed265
egmp7 <deweyerick@gmail.com>
2023-02-20 13:48:16 -0600

    main carousel functionality

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -1,23 +1,41 @@
 
 
 $(document).ready(function(){
-
-    containerWidth = $(".container").width();
-
-    //$("#rect").width(containerWidth*0.9);
-
-
-    // height
-    windowHeight = $(document).height();
-    navHeight = $("nav").outerHeight();
-    footerHeight = $("footer").outerHeight();
-    mainHeight = windowHeight - navHeight - footerHeight;
-
-    $("main").height(mainHeight);
-
-    $("modal").height(mainHeight * 0.9);
-    $("modal").width(containerWidth *0.9);
-
-
+    
+    setResponsiveComponents(getMainWidth(),getMainHeight());
 
 });
+
+/**Gets the width of the container */
+getMainWidth = function ()
+{
+    return $(".container").width();
+}
+
+/** Gets the width of the main component */
+getMainHeight = function ()
+{
+    let windowHeight = $(document).height();
+    let navHeight = $("nav").outerHeight();
+    let footerHeight = $("footer").outerHeight();
+    
+    return windowHeight - navHeight - footerHeight;
+}
+
+/**Sets width and height of components */
+setResponsiveComponents = function (w,h)
+{
+    $("main").height(h);
+    
+    // main components
+    $(".block1").width(w/2);
+    $(".block1").height(h);
+    $(".block2").width(w/2);
+    $(".block2").height(h);
+    $(".stick").height(h * 0.8);
+    
+    // modal component
+    $("modal").height(h * 0.95);
+    $("modal").width(w);
+
+}

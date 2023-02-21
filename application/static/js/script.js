22a01d1165fded3396e57f3b2fd82f420ed696c2
egmp7 <deweyerick@gmail.com>
2023-02-21 12:25:18 -0600

    main buttons

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -46,3 +46,51 @@ setResponsiveComponents = function (w,h)
     $("modal").width(w);
 
 }
+
+// events
+$( "#aboutButton" ).click(() => 
+{
+    showModal()
+    $( "#about" ).css("display", "block");
+});
+
+$( "#projectsButton" ).click(() => 
+{
+    showModal()
+    $( "#projects" ).css("display", "block");
+});
+
+$( "#servicesButton" ).click(function() {
+    showModal()
+    $( "#services" ).css("display", "block");
+});
+
+$( "#contactButton" ).click(function() {
+    showModal()
+    $( "#contact" ).css("display", "block");
+});
+$( "#closeModal" ).click(function() {
+    showMain()
+    $( "#about" ).css("display", "none");
+    $( "#projects" ).css("display", "none");
+    $( "#services" ).css("display", "none");
+    $( "#contact" ).css("display", "none");
+});
+
+/** Hides the main componets and shows the modal component */
+showModal = function()
+{
+    $("modal").css("display", "block");
+    $(".block1").css("display", "none");
+    $(".block2").css("display", "none");
+    $(".stick").css("display", "none");   
+}
+
+/** Hides the modal componet and shows the main components */
+showMain = function()
+{
+    $("modal").css("display", "none");
+    $(".block1").css("display", "block");
+    $(".block2").css("display", "block");
+    $(".stick").css("display", "block");
+}

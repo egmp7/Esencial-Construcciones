325adefb9a80e2b748ac20ff58a81e05087a2816
egmp7 <deweyerick@gmail.com>
2023-02-20 21:33:12 -0600

    greeting component

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -22,17 +22,24 @@ getMainHeight = function ()
     return windowHeight - navHeight - footerHeight;
 }
 
-/**Sets width and height of components */
+/**Sets width and height of components
+ * @param w width of the main component
+ * @param h height of the main component
+ */
 setResponsiveComponents = function (w,h)
 {
     $("main").height(h);
     
     // main components
-    $(".block1").width(w/2);
+    $(".block1").width(w / 2);
     $(".block1").height(h);
-    $(".block2").width(w/2);
+    $(".block2").width(w / 2);
     $(".block2").height(h);
     $(".stick").height(h * 0.8);
+
+    // block 1 components
+    $(".greeting").width(w / 2 * 0.85)
+    $(".greeting").height(h * 0.9)
     
     // modal component
     $("modal").height(h * 0.95);

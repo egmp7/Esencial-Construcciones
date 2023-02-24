f351f41df4422a7ef4ae25d76fedbead50782722
egmp7 <deweyerick@gmail.com>
2023-02-24 14:27:20 -0600

    refactoring layouts

--- application/static/js/componentsLayout.js
+++ application/static/js/componentsLayout.js
@@ -1,21 +1,87 @@
 
-$(document).ready(function(){
-    setComponentsPosition();
+/** This document draws tthe component of the app */
 
+$(document).ready(function(){
+    drawMainLayout();
 });
 
 window.addEventListener("resize", function(){
-
-    setComponentsPosition();
-
+    drawMainLayout();
 });
 
-/** Gets the width of the container */
+/** Draws components in main container */
+drawMainLayout = function ()
+{
+    const MAIN_WINDOW_WIDTH = getMainWidth();
+    const MAIN_WINDOW_HEIGHT = getMainHeight();
+
+    var x;  // grid variable
+    var y;  // grid variable
+    var scale;
+
+    $("main").height(MAIN_WINDOW_HEIGHT);
+
+        x = MAIN_WINDOW_WIDTH/2;
+
+        $(".block1").css({
+            width: x, 
+            height: MAIN_WINDOW_HEIGHT})
+
+            scale = 0.85
+            $(".greeting").css({
+                width: x * scale,
+                height: MAIN_WINDOW_HEIGHT * scale})
+
+        $(".block2").css({
+            width: x,
+            height: MAIN_WINDOW_HEIGHT
+        })
+
+            x = MAIN_WINDOW_WIDTH / 4;
+            y = MAIN_WINDOW_HEIGHT / 5;
+
+            $("#aboutButton").css({
+                top: y - $("#aboutButton").height()/2, 
+                left: x - $("#aboutButton").width()/2})
+            $("#projectsButton").css({
+                top: y*2 - $("#projectsButton").height()/2, 
+                left: x - $("#projectsButton").width()/2})
+            $("#servicesButton").css({
+                top: y*3 - $("#servicesButton").height()/2, 
+                left: x - $("#servicesButton").width()/2})
+            $("#contactButton").css({
+                top: y*4 - $("#contactButton").height()/2, 
+                left: x - $("#contactButton").width()/2})
+        
+        scale = 0.8
+        $(".stick").height(MAIN_WINDOW_HEIGHT * scale);
+
+        $(".modal").css({
+            width: MAIN_WINDOW_WIDTH,
+            height:MAIN_WINDOW_HEIGHT
+        })
+
+            const titleHeight = 42;
+            x = MAIN_WINDOW_WIDTH / 4;
+            y = (MAIN_WINDOW_HEIGHT - titleHeight) / 4;
+        
+            $("#projectsList").css({left:0,             top:0, 
+                                    width: x,           height: y * 4,});
+            $("#projectView").css({ left: x,            top: 0,
+                                    width: x * 3,       height: y * 3});
+            $("#projectsPreview").css({ left: x,        top: y * 3, 
+                                        width: x * 3,   height: y});
+
+            
+
+}
+
+/** gets the width of the main component */
 getMainWidth = function ()
 {
     return $(".container").width();
 }
-/** Gets the width of the main component */
+/** gets the height of the main component */
 getMainHeight = function ()
 {
     let windowHeight = $(window).height();
@@ -23,53 +89,6 @@ getMainHeight = function ()
     let footerHeight = $("footer").outerHeight();
     
     return windowHeight - navHeight - footerHeight;
-}
-
-/** Sets positions of components */
-setComponentsPosition = function ()
-{
-    var w = getMainWidth();
-    var h = getMainHeight();
-
-    console.log(h)
-    $("main").height(h);
-    
-    // main components
-    $(".block1").width(w / 2);
-    $(".block1").height(h);
-    $(".block2").width(w / 2);
-    $(".block2").height(h);
-    $(".stick").height(h * 0.8);
-    // block 1 components
-    $(".greeting").width(w / 2 * 0.85)
-    $(".greeting").height(h * 0.7)
-    
-    // modal component
-    $(".modal").height(h);
-    $(".modal").width(w);
-    
-    // main buttons
-    var x = w / 4;
-    var y = h / 5;
-    $("#aboutButton").css({top: y - $("#aboutButton").height()/2, 
-                           left: x - $("#aboutButton").width()/2})
-    $("#projectsButton").css({top: y*2 - $("#projectsButton").height()/2, 
-                              left: x - $("#projectsButton").width()/2})
-    $("#servicesButton").css({top: y*3 - $("#servicesButton").height()/2, 
-                              left: x - $("#servicesButton").width()/2})
-    $("#contactButton").css({top: y*4 - $("#contactButton").height()/2, 
-                             left: x - $("#contactButton").width()/2})
-
-    // projects component
-    var w = getMainWidth() / 4;
-    var h = (getMainHeight() - 42) / 4;
-
-    $("#projectsList").css({left:0,             top:0, 
-                            width: w,           height: h * 4,});
-    $("#projectView").css({ left: w,            top: 0,
-                            width: w * 3,       height: h * 3});
-    $("#projectsPreview").css({ left: w,        top: h * 3, 
-                                width: w * 3,   height: h});
 }
 
 
